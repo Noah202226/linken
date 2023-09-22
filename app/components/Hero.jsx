f58a0d79dc500749fa55e469ef5ffd65a7666af5
Noa Ligpitan <noaligpitan26@gmail.com>
2023-09-23 07:10:32 +0800

    initial layout

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,22 +1,45 @@
+"use client";
+import useTheme from "../stores/useTheme";
+
 const Hero = () => {
+  const { theme } = useTheme((state) => state);
   return (
     <div
-      className={`hero hero-height `}
+      className={`hero h-screen `}
       style={{
         backgroundImage:
           "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
       }}
+      data-theme={theme}
     >
+      <video
+        autoPlay
+        muted
+        loop
+        className="absolute inset-0 w-full h-full object-cover"
+      >
+        <source src="hero-bg.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+        <div className="w-full  flex items-center justify-between">
+          <div className="">
+            <h1 className="mb-5 text-5xl font-bold">
+              Empowering Innovation through Cutting-Edge Software
+            </h1>
+
+            <button className="btn btn-primary">Explore Our Services</button>
+          </div>
+          <p className="mb-5 w-11/12">
+            We specialize in crafting software solutions that drive innovation,
+            transform businesses, and elevate user experiences. Our mission is
+            to empower organizations with technology that not only meets their
+            needs but also sets new standards for excellence. With a relentless
+            commitment to quality and innovation, we're your partners in
+            achieving digital success. Explore our services and discover how we
+            can turn your ideas into reality.
           </p>
-          <button className="btn btn-primary">Get Started</button>
         </div>
       </div>
     </div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,11 @@
+import Carousel from "./components/Carousel";
 import Hero from "./components/Hero";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Carousel />
+    </>
+  );
 }
